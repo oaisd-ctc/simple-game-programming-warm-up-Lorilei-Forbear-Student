857f5c954a5ffbd6ad4c06aef38d1007f8706d60
Lorilei-Forbear-Student <207830@student.oaisd.org>
2025-01-16 12:54:10 +0000

    added endings and game over

--- Program.cs
+++ Program.cs
@@ -37,31 +37,59 @@ public class Program
         int playerScore = 0;
         int rivalScore = 0;
         int roundNumber = 0;
-        roundNumber++;
 
-        Console.WriteLine($"Round {roundNumber}");
-        int rivalRoll = RollDice();
-        Console.WriteLine($"Rival rolled a {rivalRoll}");
-        Console.WriteLine("Press any key to roll the dice...");
-        Console.ReadKey(true);
-        int playerRoll = RollDice();
-        Console.WriteLine($"You rolled a {playerRoll}");
-
-        if(rivalRoll > playerRoll)
+        while(roundNumber <= 10)
         {
-            Console.WriteLine("The Rival won this round.");
-            rivalScore++;
+            Console.WriteLine($"Round {roundNumber}");
+            int rivalRoll = RollDice();
+            Console.WriteLine($"Rival rolled a {rivalRoll}");
+            Console.WriteLine("Press any key to roll the dice...");
+            Console.ReadKey(true);
+            int playerRoll = RollDice();
+            Console.WriteLine($"You rolled a {playerRoll}");
+
+            if(rivalRoll > playerRoll)
+            {
+                Console.WriteLine("The Rival won this round.");
+                rivalScore++;
+            }
+            else if(playerRoll > rivalRoll)
+            {
+                Console.WriteLine("You won this round.");
+                playerScore++;
+            }
+            else
+            {
+                Console.WriteLine("This round was a draw!");
+            }
+
+            Console.WriteLine($"The score is now - You: {playerScore}. Rival: {rivalScore}.");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+            Console.WriteLine("");
+            roundNumber++;
         }
-        else if(playerScore > rivalScore)
+            Console.WriteLine("Game over.");
+            Console.WriteLine($"The score is now: You : {playerScore}. Rival : {rivalScore}.");
+        
+        if(playerScore > rivalScore)
         {
-            Console.WriteLine("You won this round.");
-            playerScore++;
+            Console.WriteLine("You won!");
+        }
+        else if(rivalScore > playerScore)
+        {
+            Console.WriteLine("Rival Won.");
         }
         else
         {
-            Console.WriteLine("This round was a draw!");
+            Console.WriteLine("It was a draw!");
         }
 
-        Console.WriteLine($"The score is - {playerScore} to {rivalScore}");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(true);
+            Console.WriteLine("");
+            Console.WriteLine("Dice Game was closed.");
+
     }
+
 }
